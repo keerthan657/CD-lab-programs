be98e53bd1712db9c3544a4b97a86dc744ec515a
Keerthan Kumar <keerthankk212001@gmail.com>
2022-07-20 22:25:10 +0530

    Update srparser.c

--- 5/srparser.c
+++ 5/srparser.c
@@ -1,101 +1,74 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
 
-int z = 0, i = 0, j = 0, c = 0;
-char a[16], ac[20], stk[15], act[10];
+int i=0,j=0,z=0,len=0;
+char stk[20], input[20];
 
 void check()
 {
-	// Copying string to be printed as action
-    strcpy(ac,"REDUCE TO E -> "); 
-     
-    // c=length of input string
-    for(z = 0; z < c; z++)
+    int c=len;
+    for(z=0; z<c; z++)
     {
-        //checking for producing rule E->4
-        if(stk[z] == 'c')
+        if(stk[z]=='c')
         {
-            printf("%sc", ac);
+            printf("reduced by S->c");
             stk[z] = 'S';
-            stk[z + 1] = '\0';
-             
-            //printing action
-            printf("\n$%s\t\t%s$\t", stk, a);
+            stk[z+1] = '\0';
+            printf("\n$%s\t\t%s$\t\t", stk, input);
         }
     }
-         
-    for(z = 0; z < c - 2; z++)
-    {
-        //checking for another production
-        if(stk[z] == 'a' && stk[z + 1] == 'S' &&
-                                stk[z + 2] == 'a')
-        {
-            printf("%saSa", ac);
-            stk[z] = 'S';
-            stk[z + 1] = '\0';
-            stk[z + 2] = '\0';
-            printf("\n$%s\t\t%s$\t", stk, a);
-            i = i - 2;
-        }
-         
-    }
-         
     for(z=0; z<c-2; z++)
     {
-        //checking for E->3E3
-        if(stk[z] == 'b' && stk[z + 1] == 'S' &&
-                                stk[z + 2] == 'b')
+        if(stk[z]=='a' && stk[z+1]=='S' && stk[z+2]=='a')
         {
-            printf("%sbSb", ac);
-            stk[z]='S';
-            stk[z + 1]='\0';
-            stk[z + 1]='\0';
-            printf("\n$%s\t\t%s$\t", stk, a);
-            i = i - 2;
+            printf("reduced by S->aSa");
+            stk[z] = 'S';
+            stk[z+1] = '\0';
+            stk[z+2] = '\0';
+            printf("\n$%s\t\t%s$\t\t", stk, input);
+            i=i-2;
         }
     }
-    return ; //return to main
+    for(z=0; z<c-2; z++)
+    {
+        if(stk[z]=='b' && stk[z+1]=='S' && stk[z+2]=='b')
+        {
+            printf("reduced by S->bSb");
+            stk[z] = 'S';
+            stk[z+1] = '\0';
+            stk[z+2] = '\0';
+            printf("\n$%s\t\t%s$\t\t", stk, input);
+            i=i-2;
+        }
+    }
 }
 
 int main()
 {
-	printf("GRAMMAR is -\nS->aSa \nS->bSb \nS->c\n");
-    
+    printf("grammar is S->aSa|bSb|c\n");
     printf("enter input: ");
-	scanf("%s", a);
+    scanf("%s", input);
 
-    // strlen(a) will return the length of a to c
-    c=strlen(a);
-     
-    strcpy(act,"SHIFT");
-     
-    printf("\nstack \t\t input \t\t action");
-     
-    printf("\n$\t\t%s$\t", a);
-     
-    for(i = 0; j < c; i++, j++)
+    len = strlen(input);
+    char action[20] = "Shift";
+
+    printf("STACK\t\tINPUT\t\tACTION");
+    printf("\n$\t\t%s$\t\t", input);
+
+    for(i=0; j<len; i++,j++)
     {
-        printf("%s", act);
-         
-        // Pushing into stack
-        stk[i] = a[j];    
-        stk[i + 1] = '\0';
-         
-        a[j]=' ';
-         
-        // Printing action
-        printf("\n$%s\t\t%s$\t", stk, a);
-         
+        printf("%s", action);
+        stk[i] = input[j];
+        stk[i+1] = '\0';
+        input[j] = ' ';
+        printf("\n$%s\t\t%s$\t\t", stk, input);
         check();
     }
-     
     check();
-     
-    // if top of the stack is E(starting symbol)
-    // then it will accept the input
-    if(stk[0] == 'S' && stk[1] == '\0')
-        printf("Accept\n");
-    else //else reject
-        printf("Reject\n");
+    
+    if(stk[0]=='S' && stk[1]=='\0')
+        printf("ACCEPT\n");
+    else
+        printf("REJECT\n");
+    return 0;
 }
